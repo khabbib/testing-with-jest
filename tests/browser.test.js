9d4efb00b022dab4dfe88b3be2eb06d9efc8dde9
khabib <habib.pakdel1121@gmail.com>
2024-05-08 00:14:38 +0200

    stack and browser tests

---
+++ tests/browser.test.js
@@ -0,0 +1,44 @@
+const { Builder, By, until } = require('selenium-webdriver');
+require('geckodriver');
+
+const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+jest.setTimeout(1000 * 60 * 5); // 5 minuter
+
+// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
+beforeAll(async () => {
+console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
+
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
+
+test('The stack should be empty in the beginning', async () => {
+	let stack = await driver.findElement(By.id('top_of_stack')).getText();
+	expect(stack).toEqual("n/a");
+});
+
+describe('Clicking "Pusha till stacken"', () => {
+	it('should open a prompt box', async () => {
+		let push = await driver.findElement(By.id('push'));
+		await push.click();
+		let alert = await driver.switchTo().alert();
+		await alert.sendKeys("Bananer");
+		await alert.accept();
+	});
+});
+
+// Egen test
+test('By clicking on "Poppa stacken!" it should delete the last item', async () => {
+	let pop = await driver.findElement(By.id('pop'));
+	await pop.click();
+	let alert = await driver.switchTo().alert();
+	let alertText = await alert.getText();
+	await alert.accept();
+	expect(alertText).toEqual("Tog bort Bananer"); // medvetet fel, sista stacken bör vara "Bananer"
+});
